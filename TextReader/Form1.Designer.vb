6815f8f49b6dc17d0378a3bf49dd98f70bd376dd
Diego Amaro Castellucchio <castellucchiosoftware@gmail.com>
2021-06-10 14:59:22 -0300

    Primeira versão do meu leitor de arquivos

---
+++ TextReader/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNomeArquivo = New System.Windows.Forms.Label()
+        Me.Abrir = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'lblNomeArquivo
+        '
+        Me.lblNomeArquivo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblNomeArquivo.AutoSize = True
+        Me.lblNomeArquivo.Location = New System.Drawing.Point(13, 13)
+        Me.lblNomeArquivo.Name = "lblNomeArquivo"
+        Me.lblNomeArquivo.Size = New System.Drawing.Size(104, 13)
+        Me.lblNomeArquivo.TabIndex = 0
+        Me.lblNomeArquivo.Text = "Caminho do arquivo:"
+        '
+        'Abrir
+        '
+        Me.Abrir.Location = New System.Drawing.Point(16, 43)
+        Me.Abrir.Name = "Abrir"
+        Me.Abrir.Size = New System.Drawing.Size(75, 23)
+        Me.Abrir.TabIndex = 1
+        Me.Abrir.Text = "Abrir"
+        Me.Abrir.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtOutput.Location = New System.Drawing.Point(16, 72)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(694, 475)
+        Me.txtOutput.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(722, 559)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.Abrir)
+        Me.Controls.Add(Me.lblNomeArquivo)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Leitor de arquivos de texto - Feito por Diego Castellucchio"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblNomeArquivo As System.Windows.Forms.Label
+    Friend WithEvents Abrir As System.Windows.Forms.Button
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ TextReader/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Private Sub Abrir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Abrir.Click
+        Dim sr As StreamReader
+        If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Try
+                sr = New StreamReader(OpenFileDialog1.FileName)
+                lblNomeArquivo.Text += " " + OpenFileDialog1.FileName
+                txtOutput.Text = sr.ReadToEnd()
+            Catch ex As Exception
+                txtOutput.Text = ex.Message
+            Finally
+                sr.Dispose()
+            End Try
+        End If
+    End Sub
+End Class
